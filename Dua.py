a9eea6b48ba35a157afd17a280c863d9cb7952b2
Dua392 <duak0295@gmail.com>
2025-04-19 19:40:46 +0500

    Add files via upload

---
+++ Dua.py
@@ -0,0 +1,65 @@
+#Q1 Write a program that calculates the sum ,difference ,product and average of three numbers?
+
+
+a = 10
+b = 5
+c = 2
+
+sum_result = a + b + c 
+difference = a - b - c  
+product = a * b * c    
+average = sum_result / 3
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Average:", average)
+
+print("output")
+sum:17
+difference:3
+product:100
+average:5.666666666666667
+
+#Q 02: Area of Triangle, Square, Circle, and Rectangle
+
+# Triangle
+base = 10
+height = 5
+triangle_area = 0.5 * base * height
+print("Area of Triangle:", triangle_area)
+
+# Square
+side = 4
+square_area = side * side
+print("Area of Square:", square_area)
+
+# Circle 
+radius = 7
+circle_area = 3.14 * radius ** 2
+print("Area of Circle:", circle_area)
+
+# Rectangle
+length = 8
+width = 3
+rectangle_area = length * width
+print("Area of Rectangle:", rectangle_area)
+
+print("output")
+print("area of triangle:25.0")
+print("area of square:16")
+print("area of circle:153.86")
+print("area of rectangle:24")
+
+
+#Q 03: Swap Values of Two Variables
+
+x =15
+y = 30
+
+x, y = y, x
+
+print("After swapping:")
+print("x =", x)
+print("y =", y)
+
